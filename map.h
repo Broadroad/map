757dd48f93dcb7a47f0869f5d2378284715da97c
zhang kang <zkdnfcf@163.com>
2016-05-31 22:02:05 +0800

    2016年 5月31日 星期二 22时02分05秒 CST

---
+++ map.h
@@ -0,0 +1,95 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2016 zkdnfcf, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file hash.h
+ * @author zk(zkdnfc@163.com)
+ * @date 2016/05/31 18:26:01
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+#ifndef _MAP_H
+#define _MAP_H
+
+#include "rbtree.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+struct map {
+    struct rb_node node;
+    char *key;
+    char *val;
+};
+
+typedef struct map map_t;
+typedef struct rb_root root_t;
+typedef struct rb_node rb_node_t;
+
+map_t *get(root_t *root, char *str) {
+   rb_node_t *node = root->rb_node; 
+   while (node) {
+        map_t *data = container_of(node, map_t, node);
+
+        //compare between the key with the keys in map
+        int cmp = strcmp(str, data->key);
+        if (cmp < 0) {
+            node = node->rb_left;
+        }else if (cmp > 0) {
+            node = node->rb_right;
+        }else {
+            return data;
+        }
+   }
+   return NULL;
+}
+
+int put(root_t *root, char* key, char* val) {
+    map_t *data = (map_t*)malloc(sizeof(map_t));
+    data->key = (char*)malloc(sizeof(key));
+    strcpy(data->key, key);
+    data->val = (char*)malloc(sizeof(val));
+    strcpy(data->val, val);
+    rb_node_t **new_node = &(root->rb_node), *parent = NULL;
+    while (*new_node) {
+        map_t *this_node = container_of(*new_node, map_t, node);
+        int result = strcmp(key, this_node->key);
+        parent = *new_node;
+
+        if (result < 0) {
+            new_node = &((*new_node)->rb_left);
+        }else if (result > 0) {
+            new_node = &((*new_node)->rb_left);
+        }else {
+            return 0;
+        }
+    }
+
+    rb_link_node(&data->node, parent, new_node);
+    rb_insert_color(&data->node, root);
+
+    return 1;
+}
+
+void my_free(map_t *node){
+    if (node != NULL) {
+        if (node->key != NULL) {
+            free(node->key);
+            node->key = NULL;
+            free(node->val);
+            node->val = NULL;
+    }
+        free(node);
+        node = NULL;
+    }
+}
+
+#endif  //_MAP_H
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
